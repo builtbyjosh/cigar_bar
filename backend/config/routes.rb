a82b0a2a0713aab5c2ca580e6f4afad56ed9f7af
builtbyjosh <pc.refuge@gmail.com>
2021-03-08 22:47:14 -0600

    add show to routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cigars, only: [:index]
-  resources :styles, only: [:index]
+  resources :styles, only: [:index, :show]
+  resources :cigars, only: [:index, :show]
 end
