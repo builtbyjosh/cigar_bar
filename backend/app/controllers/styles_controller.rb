788ece1d00990a38fd352ebfd5cd8d1a29180602
builtbyjosh <pc.refuge@gmail.com>
2021-03-08 22:57:38 -0600

    added show route to styles

--- backend/app/controllers/styles_controller.rb
+++ backend/app/controllers/styles_controller.rb
@@ -5,4 +5,8 @@ class StylesController < ApplicationController
         render json: styles
     end
     
+    def show
+        style = Style.find(params[:id])
+        render json: style
+    end
 end
