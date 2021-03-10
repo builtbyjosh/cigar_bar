73185eb0c1991740ed17afbbbe07983e2fba5dc3
builtbyjosh <pc.refuge@gmail.com>
2021-03-09 21:49:08 -0600

    add event listeners for nav items

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 const cigarList = document.querySelector(".cigar-list")
-
+const allCigars = document.querySelector(".all-cigars")
+const allStyles = document.querySelector(".all-styles")
+const newCigar = document.querySelector(".new-cigar")
+const cardHeader = document.querySelector(".card-header")
+const cardItem = cardHeader.getElementsByClassName("nav-link")
 
 fetch('http://localhost:3000/cigars')
     .then(res => res.json())
     .then((data) => renderCigars(data))
 
-    
 
 const renderCigars = function(cigars){
     
@@ -19,3 +22,30 @@ const renderCigars = function(cigars){
         cigarList.appendChild(cigarLi)
     });
 }
+
+for (let i = 0; i < cardItem.length; i++) {
+    cardItem[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active")
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "")
+      }
+      this.className += " active";
+    });
+  }
+
+allCigars.addEventListener("click",(e) =>{
+    e.preventDefault
+    console.log("cigars clicked")
+})
+
+allStyles.addEventListener("click",(e) =>{
+    e.preventDefault
+    console.log("styles clicked")
+})
+
+newCigar.addEventListener("click",(e) =>{
+    e.preventDefault
+    console.log("new cigar form clicked")
+})
+
+
