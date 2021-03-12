56fd18cf8aedd73b50360ef3a685416dd3641e9c
builtbyjosh <pc.refuge@gmail.com>
2021-03-12 16:04:34 -0600

    added create route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :styles, only: [:index, :show]
-  resources :cigars, only: [:index, :show]
+  resources :cigars, only: [:index, :show, :create]
 end
