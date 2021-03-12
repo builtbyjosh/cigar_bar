c17df9ee059ee17d9e6a13fe7c255ae51467c70d
builtbyjosh <pc.refuge@gmail.com>
2021-03-12 16:04:59 -0600

    added cigar_params

--- backend/app/controllers/cigars_controller.rb
+++ backend/app/controllers/cigars_controller.rb
@@ -6,10 +6,21 @@ class CigarsController < ApplicationController
     end
 
     def show
-        cigar = Cigar.find(params[:id])
-        
+        cigar = Cigar.find(params[:id])        
         render json: CigarSerializer.new(cigar)
+    end
 
+    def create
+        binding.pry
+        cigar = Cigar.new(cigar_params)
+        render json: cigar
+        
+    end
+
+    private
+
+    def cigar_params
+        params.require(:cigar).permit(:name, :description, :price)
     end
 
 end
