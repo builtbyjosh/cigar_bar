59873d144c4b5597c59064b7f10b0737fb9e3766
builtbyjosh <pc.refuge@gmail.com>
2021-03-15 12:57:33 -0500

    completed style cigar list

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ const newCigar = document.querySelector(".new-cigar")
 const cardHeader = document.querySelector(".card-header")
 const cardItem = document.getElementsByClassName("nav-link")
 const styleDropdown = document.querySelector("#style-dropdown")
-const styleLi = document.querySelectorAll('.list-group-item')
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
     getCigars()
@@ -13,6 +13,78 @@ document.addEventListener('DOMContentLoaded', (e) => {
     renderAllItems(Cigar)
 })
 
+// navbar logic
+for (let i = 0; i < cardItem.length; i++) {
+    cardItem[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active")
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "")
+      }
+      this.className += " active"
+    });
+}
+
+// render cigars and styles
+function renderSingleItem(item){
+    return `<li class="list-group-item">${item.name}</ul></li>`
+}
+  
+const renderAllItems = function(classObj){
+    mainList.innerHTML = ""
+    classObj.all_items.forEach(classItem => {
+        mainList.innerHTML += renderSingleItem(classItem)
+    });
+}
+
+
+
+// All Cigar Tab
+allCigars.addEventListener("click",(e) =>{
+    e.preventDefault()
+    renderAllItems(Cigar)
+})
+
+
+
+// Cigar Styles Tab
+const cigarStyleList = function() {
+    const styleLi = document.querySelectorAll(".list-group-item")
+    styleLi.forEach(li =>{
+        li.addEventListener('click', function(){            
+            const cigarUL = "<ul class='cigar-list'></ul>"
+            const allCigarUl = document.querySelectorAll('.cigar-list')        
+            li.innerHTML += cigarUL
+            allCigarUl.forEach(li => li.innerHTML = "")
+            findCigars(li)
+        })
+    })
+}
+
+const findCigars = function(obj){
+    const list =  Cigar.all_items.filter ( i => i.style == obj.textContent)
+    createList(list, obj)
+}
+
+const createList = function(data, obj){
+    data.forEach(cigar =>{
+        const cigarList = document.querySelector(".cigar-list")        
+        const cigarItem = document.createElement('li')
+        cigarItem.innerText = cigar.name
+        obj.appendChild(cigarList)
+        cigarList.appendChild(cigarItem)
+    })
+}
+
+allStyles.addEventListener("click",(e) =>{
+    e.preventDefault()
+    renderAllItems(Style)
+    cigarStyleList()
+  
+})
+
+
+
+// form creation
 const renderForm = function(){
     mainList.innerHTML = `
     <form id="new-cigar">
@@ -44,36 +116,6 @@ const populateDropdown = function(){
     })    
 }
 
-for (let i = 0; i < cardItem.length; i++) {
-    cardItem[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("active")
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "")
-      }
-      this.className += " active"
-    });
-}
-
-function renderSingleItem(item){
-    return `<li class="list-group-item">${item.name}</ul></li>`
-}  
-const renderAllItems = function(classObj){
-    mainList.innerHTML = ""
-    classObj.all_items.forEach(classItem => {
-        mainList.innerHTML += renderSingleItem(classItem)
-    });
-}
-
-allCigars.addEventListener("click",(e) =>{
-    e.preventDefault()
-    renderAllItems(Cigar)
-})
-
-allStyles.addEventListener("click",(e) =>{
-    e.preventDefault()
-    renderAllItems(Style)    
-})
-
 newCigar.addEventListener("click",(e) =>{
     e.preventDefault()
     renderForm()
@@ -89,4 +131,14 @@ newCigar.addEventListener("click",(e) =>{
         submitCigar(formData)
     })
 })
+
+
+
+
+
+
+
+
+
+
 
