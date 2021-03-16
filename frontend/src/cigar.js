2504c0d02836dd80ebaf18f8e0f6efb472044037
builtbyjosh <pc.refuge@gmail.com>
2021-03-16 12:22:35 -0500

    add id to constructor

--- frontend/src/cigar.js
+++ frontend/src/cigar.js
@@ -3,6 +3,7 @@ class Cigar {
     static all_items = []
 
     constructor(data){
+        this.id = data.id
         this.name = data.name
         this.description = data.description
         this.price = data.price
